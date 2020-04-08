c30ac780e692a5adb6d04609e4db4f70b0109410
DieterClaessens67 <dieterclaessens3@hotmail.com>
2020-04-08 09:14:05 +0200

    deelopdracht2 klaar!

--- web/index.jsp
+++ web/index.jsp
@@ -67,6 +67,86 @@
 				<option value="10">10</option>
 			</select>
 			<input type="button" id="commentButton" value="Ok">
+		<div id="comments1"></div>
+		<p>Wat ben je van plan om te doen vandaag?</p>
+			<label for="name1">Name</label>
+		<input type="text" name="name1" id="name1">
+		<label for="comment1">Comment</label>
+		<input id="comment1" name="comment1" type="text">
+		<label for="score1">Score</label>
+		<select id="score1">
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+			<option value="7">7</option>
+			<option value="8">8</option>
+			<option value="9">9</option>
+			<option value="10">10</option>
+		</select>
+		<input type="button" id="commentButton1" value="Ok">
+		<div id="comments2"></div>
+		<p>Naar	welke muziek ben je momenteel aan het luisteren?</p>
+		<label for="name2">Name</label>
+		<input type="text" id="name2" name="name2">
+		<label for="comment2">Comment</label>
+		<input type="text" id="comment2" name="comment2">
+		<label for="score2">Score</label>
+		<select id="score2">
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+			<option value="7">7</option>
+			<option value="8">8</option>
+			<option value="9">9</option>
+			<option value="10">10</option>
+		</select>
+		<input type="button" id="commentButton2" value="Ok">
+		<div id="comments3"></div>
+		<p>Wat zijn de examenvragen voor het vak Web4?</p>
+		<label for="name3">Name</label>
+		<input type="text" id="name3" name="name3">
+		<label for="comment3">Comment</label>
+		<input type="text" id="comment3" name="comment3">
+		<label for="score3">Score</label>
+			<select id="score3">
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
+				<option value="6">6</option>
+				<option value="7">7</option>
+				<option value="8">8</option>
+				<option value="9">9</option>
+				<option value="10">10</option>
+			</select>
+		<input type="button" id="commentButton3" value="Ok">
+		<div id="comments4"></div>
+		<p>Wat is jullie favoriete anime?</p>
+		<label for="name4">Name</label>
+		<input type="text" id="name4" name="name4">
+		<label for="comment4">Comment</label>
+		<input type="text" id="comment4" name="comment4">
+		<label for="score4">Score</label>
+		<select id="score4">
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+			<option value="7">7</option>
+			<option value="8">8</option>
+			<option value="9">9</option>
+			<option value="10">10</option>
+		</select>
+		<input type="button" id="commentButton4" value="Ok">
 	</section>
 	</main>
 
--- web/js/comment.js
+++ web/js/comment.js
@@ -1,18 +1,46 @@
 let commentSection = document.getElementById("comments");
+let commentSection1 = document.getElementById("comments1");
+let commentSection2 = document.getElementById("comments2");
+let commentSection3 = document.getElementById("comments3");
+let commentSection4 = document.getElementById("comments4");
+
 let websocket;
+
 let commentButton = document.getElementById("commentButton");
-commentButton.addEventListener("click",comment);
+let commentButton1 = document.getElementById("commentButton1");
+let commentButton2 = document.getElementById("commentButton2");
+let commentButton3 = document.getElementById("commentButton3");
+let commentButton4 = document.getElementById("commentButton4");
+
+commentButton.addEventListener("click",send);
+commentButton1.addEventListener("click",send1);
+commentButton2.addEventListener("click",send2);
+commentButton3.addEventListener("click",send3);
+commentButton4.addEventListener("click",send4);
+
 window.addEventListener("load",openSocket);
 window.addEventListener("close",closeSocket);
 
-function comment() {
-    send();
-}
 
 function openSocket() {
     websocket = new WebSocket("ws://localhost:8080/echo");
     websocket.onmessage = function(event){
-        writeResponse(event.data);
+        if(event.data.charAt(0) === '0') {
+            let text = event.data.substring(1);
+            writeResponse(text);
+        }
+        if(event.data.charAt(0) === '1'){
+            writeResponse1(event.data.substring(1));
+        }
+        if(event.data.charAt(0) === '2'){
+            writeResponse2(event.data.substring(1));
+        }
+        if(event.data.charAt(0)=== '3'){
+            writeResponse3(event.data.substring(1));
+        }
+        if(event.data.charAt(0)==='4'){
+            writeResponse4(event.data.substring(1));
+        }
     };
 }
 
@@ -20,12 +48,60 @@ function writeResponse(text){
     commentSection.innerHTML += "<br/>" + text;
 }
 
+function writeResponse1(text) {
+    commentSection1.innerHTML += "<br/>" + text;
+}
+
+function writeResponse2(text) {
+    commentSection2.innerHTML += "<br/>" + text;
+}
+
+function writeResponse3(text) {
+    commentSection3.innerHTML += "<br/>" + text;
+}
+
+function writeResponse4(text) {
+    commentSection4.innerHTML += "<br/>" + text;
+}
+
 function send(){
     let name = document.getElementById("name").value;
     let comment = document.getElementById("comment").value;
     let score = document.getElementById("score").value;
-    let text = name+": "+comment+" "+score;
+    let text = "0"+name+": "+comment+" "+score;
     websocket.send(text);
+}
+
+function send1() {
+    let name1 = document.getElementById("name1").value;
+    let comment1 = document.getElementById("comment1").value;
+    let score1 = document.getElementById("score1").value;
+    let text1 = "1"+name1+": "+comment1+" "+score1;
+    websocket.send(text1);
+}
+
+function send2() {
+    let name2 = document.getElementById("name2").value;
+    let comment2 = document.getElementById("comment2").value;
+    let score2 = document.getElementById("score2").value;
+    let text2 = "2"+name2+": "+comment2+" "+score2;
+    websocket.send(text2);
+}
+
+function send3() {
+    let name3 = document.getElementById("name3").value;
+    let comment3 = document.getElementById("comment3").value;
+    let score3 = document.getElementById("score3").value;
+    let text3 = "3"+name3+": "+comment3+" "+score3;
+    websocket.send(text3);
+}
+
+function send4() {
+    let name4 = document.getElementById("name4").value;
+    let comment4 = document.getElementById("comment4").value;
+    let score4 = document.getElementById("score4").value;
+    let text4 = "4"+name4+": "+comment4+" "+score4;
+    websocket.send(text4);
 }
 
 function closeSocket(){
